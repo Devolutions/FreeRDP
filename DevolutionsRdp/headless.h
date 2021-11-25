25f912edcad337b6f947157d7d72f02df19a17c6
Richard Markiewicz <rmarkiewicz@devolutions.net>
2021-11-24 19:56:14 -0800

    devolutionsrdp: - remove broken Windows shmem implementation - fix warning in cursor.c - fix issues with bindings

--- DevolutionsRdp/headless.h
+++ DevolutionsRdp/headless.h
@@ -3,11 +3,7 @@
 
 #include <freerdp/api.h>
 
-FREERDP_API void* csharp_create_shared_buffer(char* name, int size);
-#ifdef _WIN32
-FREERDP_API void csharp_destroy_shared_buffer(void* hMapFile);
-#else
+FREERDP_API BOOL csharp_create_shared_buffer(char* name, int size);
 FREERDP_API void csharp_destroy_shared_buffer(char* name);
-#endif
 
 #endif /* CS_HEADLESS_H_ */
