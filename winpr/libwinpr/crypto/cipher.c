5625f30a46f5bdf24316a2e4f7f5c051ce213b2f
Richard Markiewicz <rmarkiewicz@devolutions.net>
2023-08-03 14:56:55 -0400

    freerdp: fix Windows build

--- winpr/libwinpr/crypto/cipher.c
+++ winpr/libwinpr/crypto/cipher.c
@@ -768,7 +768,7 @@ void winpr_Cipher_Free(WINPR_CIPHER_CTX* ctx)
  */
 
 int winpr_Cipher_BytesToKey(int cipher, WINPR_MD_TYPE md, const void* salt, const void* data,
-                            size_t datal, size_t count, void* key, void* iv)
+                            size_t datal, size_t count, BYTE* key, BYTE* iv)
 {
 	/**
 	 * Key and IV generation compatible with OpenSSL EVP_BytesToKey():
@@ -859,7 +859,7 @@ int winpr_Cipher_BytesToKey(int cipher, WINPR_MD_TYPE md, const void* salt, cons
 					break;
 
 				if (key)
-					*(BYTE*)(key++) = md_buf[i];
+					*(key++) = md_buf[i];
 
 				nkey--;
 				i++;
@@ -877,7 +877,7 @@ int winpr_Cipher_BytesToKey(int cipher, WINPR_MD_TYPE md, const void* salt, cons
 					break;
 
 				if (iv)
-					*(BYTE*)(iv++) = md_buf[i];
+					*(iv++) = md_buf[i];
 
 				niv--;
 				i++;
