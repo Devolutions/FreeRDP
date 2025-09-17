4222ab6e9ef1f4f866fba30402c9fa814c2e613c
Richard Markiewicz <rmarkiewicz@devolutions.net>
2025-09-17 14:33:39 -0400

    freerdp: add NOW proto DVC implementation (#28)
    Adds an implementation of the Devolutions::Now::Agent DVC as a generic
    DVC plugin.
    
    Channel is implemented to just forward TX and RX. Implementation details
    are handled in C#.
    
    Fix build issues with legacy RDM* channels.
    
    Cleanup a few warnings.

---
+++ include/freerdp/channels/nowproto.h
@@ -0,0 +1,18 @@
+#ifndef FREERDP_CHANNEL_NOW_PROTO_H
+#define FREERDP_CHANNEL_NOW_PROTO_H
+
+#include <winpr/wtypes.h>
+#include <freerdp/types.h>
+
+#define NOWPROTO_CHANNEL_NAME "nowproto"
+#define NOWPROTO_DVC_CHANNEL_NAME "Devolutions::Now::Agent"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* FREERDP_CHANNEL_NOW_PROTO_H */
